bb7bc4636e2ee7a10e39bb7846ec0e4f0f856ec3
Nick Nichols <nichols1991@gmail.com>
2024-09-20 16:05:36 +0000

    Add basic tests

---
+++ test/leiningen/bouncer/impl_test.clj
@@ -0,0 +1,16 @@
+(ns leiningen.bouncer.impl-test
+  (:require [clojure.test :refer :all]
+            [com.wallbrew.spoon.spec :as spoon.spec]
+            [leiningen.bouncer.impl :as sut]
+            [leiningen.bouncer.types.config :as config]))
+
+
+(deftest load-config!-test
+  (testing "load-config! should return a valid config map."
+    (is (map? (sut/load-config!)))
+    (is (spoon.spec/test-valid? ::config/config (sut/load-config!))))
+  (testing "load-config! should return a valid config map if a config file isn't found."
+    (with-redefs [sut/file-exists? (constantly false)]
+      (is (map? (sut/load-config!)))
+      (is (spoon.spec/test-valid? ::config/config (sut/load-config!))))))
+
---
+++ test/leiningen/bouncer/types/config_test.clj
@@ -0,0 +1,25 @@
+(ns leiningen.bouncer.types.config-test
+  (:require [clojure.test :refer :all]
+            [com.wallbrew.spoon.spec :as spoon.spec]
+            [leiningen.bouncer.test-util :as test-util]
+            [leiningen.bouncer.types.config :as sut]))
+
+
+(deftest generatable?-test
+  (testing "All specs can generate values that pass validation"
+    (is (test-util/generatable? ::sut/config))
+    (is (test-util/generatable? ::sut/project-rules))
+    (is (test-util/generatable? ::sut/plugins))
+    (is (test-util/generatable? ::sut/available-plugins))
+    (is (test-util/generatable? ::sut/plugin))
+    (is (test-util/generatable? ::sut/plugin-version))
+    (is (test-util/generatable? ::sut/plugin-name))
+    (is (test-util/generatable? ::sut/license))
+    (is (test-util/generatable? ::sut/rule))
+    (is (test-util/generatable? ::sut/comment))
+    (is (test-util/generatable? ::sut/reason))
+    (is (test-util/generatable? ::sut/disabled))))
+
+(deftest valid-default-config-test
+  (testing "The default config should be valid."
+    (is (spoon.spec/test-valid? ::sut/config sut/default-config))))
