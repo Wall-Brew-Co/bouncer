965354d70d4e9fd2b8e7013bcf09d0825e4dff9c
Nick Nichols <nichols1991@gmail.com>
2024-10-13 18:24:30 +0000

    Add file handling utilities

--- src/leiningen/bouncer/types/config.clj
+++ src/leiningen/bouncer/types/config.clj
@@ -8,6 +8,10 @@
   "The default name of the configuration file."
   ".bouncer/config.edn")
 
+(def ^:const backup-config-file
+  "The secondary name of the configuration file."
+  ".wallbrew/bouncer/config.edn")
+
 
 (spec/def ::disabled
   (st/spec
--- test/leiningen/bouncer/impl_test.clj
+++ test/leiningen/bouncer/impl_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [com.wallbrew.spoon.spec :as spoon.spec]
             [leiningen.bouncer.impl :as sut]
+            [leiningen.bouncer.impl.io :as io]
             [leiningen.bouncer.types.config :as config]))
 
 
@@ -10,7 +11,7 @@
     (is (map? (sut/load-config!)))
     (is (spoon.spec/test-valid? ::config/config (sut/load-config!))))
   (testing "load-config! should return a valid config map if a config file isn't found."
-    (with-redefs [sut/file-exists? (constantly false)]
+    (with-redefs [io/file-exists? (constantly false)]
       (is (map? (sut/load-config!)))
       (is (spoon.spec/test-valid? ::config/config (sut/load-config!))))))
 
