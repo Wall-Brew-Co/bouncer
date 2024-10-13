8775e4aabef16c95207a273c74c8b9fe23614aad
Nick Nichols <nichols1991@gmail.com>
2024-10-13 20:52:57 +0000

    Deep merge config

--- src/leiningen/bouncer/impl.clj
+++ src/leiningen/bouncer/impl.clj
@@ -39,6 +39,16 @@
       :else                      (do (main/info "No configuration file found. Assuming default configuration.")
                                      config/default-config))))
 
+(defn deep-merge
+  "Recursively merge two maps."
+  [m1 m2]
+  (if (and (map? m1) (map? m2))
+    (reduce (fn [acc [k v]]
+              (assoc acc k (deep-merge (get m1 k) v)))
+            m1
+            m2)
+    m2))
+
 
 (defn load-config!
   "Load the configuration file with the following precedence:
@@ -46,10 +56,13 @@
       - The configuration file in .bouncer/config.edn
       - The configuration file in .wallbrew/bouncer/config.edn
       - The default configuration
-     If the configuration is invalid, print a warning and exit."
+     If the configuration is invalid, print a warning and exit.
+
+   N.B - This function merges the configuration file with the default configuration."
   [project]
-  (let [config (select-config project)]
-    (if (spec/valid? ::config/config config)
-      config
-      (do (main/warn (format "Invalid configuration file contents: %s" (spec/explain-str ::config/config config)))
+  (let [config               (select-config project)
+        config-with-defaults (deep-merge config/default-config config)]
+    (if (spec/valid? ::config/config config-with-defaults)
+      config-with-defaults
+      (do (main/warn (format "Invalid configuration file contents: %s" (spec/explain-str ::config/config config-with-defaults)))
           (main/exit 1)))))
